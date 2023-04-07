82dbf006625b1e2f1c54fbb1f9ab207234d948cb
Angsuman Barua Pritom <angsumanbarua11@gmail.com>
2023-04-08 04:00:05 +0600

    Session creation and edit

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -13,12 +13,48 @@ class SessionController extends Controller
         return view('session.session',compact('ses'));
     }
     public function StartSession(){
+        //$last  = Session::max('id');
         $last = Session::latest()->first();
+        //dd($last);
         return view('session.new_session',compact(['last']));
     }
     public function SessionCourses(){
         $courses = Course::all();
         return view('session.start_session',compact('courses'));
+    }
+    public function StoreSession(){
+        $last = Session::latest()->first();
+        $year=$last->Year;
+        if($last->Name=="Spring"){
+            $new_name="Fall";
+        }
+        else{
+            $new_name="Spring";
+            $year++;
+        }
+        $obj = new Session();
+        $obj->Session_name=$new_name." ".$year;
+        $obj->Name=$new_name;
+        $obj->Year=$year;
+        $obj->Status=0;
+        if($obj->save()){
+            return redirect('/session');
+        }
+    }
+    public function DeactiveSession($id){
+        $ses = Session::find($id);
+        $ses->Staus=0;
+        if($ses->save()){
+            return view('session.session');
+        }
+    }
+    public function ActiveSession($id){
+        $ses = Session::find($id);
+        $ses->Staus=1;
+        if($ses->save()){
+            return view('session.session');
+        }
+        
     }
 
 }
